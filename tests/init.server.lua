78433896c176456ee09bd567176e9cb6f1df8991
David Minnerly <me@vocksel.com>
2021-12-28 09:35:40 -0800

    Fix installation through Wally

--- tests/init.server.lua
+++ tests/init.server.lua
@@ -1,4 +1,13 @@
--- run-in-roblox does not play well with .server.lua files. As such, spec.lua is
--- passed off to run-in-roblox, and this script is mounted in dev.project.json
--- so that we can run tests in Studio.
-require(script.spec)
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+
+local TestEZ = require(ReplicatedStorage.Packages.TestEZ)
+
+local results = TestEZ.TestBootstrap:run({
+	ReplicatedStorage.Packages,
+})
+
+if results.failureCount > 0 then
+	print("❌ Test run failed")
+else
+	print("✔️ All tests passed")
+end
